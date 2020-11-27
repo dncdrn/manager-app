e694e978a2885ecbddeb64bb69047abe1a4c924d
dncdrn <missnentin@gmail.com>
2020-11-28 01:45:20 +0800

    added originalpostcomment variable added sesrchbox in postlistcomments page

--- src/page/PostListWithComments.js
+++ src/page/PostListWithComments.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import PostComments from "../components/PostComments";
 import PostItem from "../components/PostItem";
+import SearchBox from "../components/SearchBox";
 import {
   getSinglePostByID,
   getSinglePostWithComments,
@@ -12,6 +13,7 @@ export default function PostListWithComments() {
 
   const [postDetail, setPostDetail] = useState();
   const [postComments, setPostComments] = useState();
+  const [originalPostComments, setOriginalPostComments] = useState();
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -22,14 +24,21 @@ export default function PostListWithComments() {
     const fetchPostWithComment = async () => {
       const postWithComments = await getSinglePostWithComments(postID);
       setPostComments(postWithComments);
+      setOriginalPostComments(postWithComments);
     };
 
     Promise.all([fetchPost(), fetchPostWithComment()]);
   }, []);
+
   return (
     <div>
       <div className="title">Post</div>
       {postDetail && <PostItem post={postDetail} />}
+      <SearchBox
+        postComments={postComments}
+        setPostComments={setPostComments}
+        originalPostComments={originalPostComments}
+      />
       {postComments &&
         postComments.map((postComment) => (
           <PostComments postComment={postComment} />
