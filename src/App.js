e21db80476e9f652d169f3c5d70fc487f298e16b
dncdrn <missnentin@gmail.com>
2020-11-28 00:37:33 +0800

    modify app.js for the routing

--- src/App.js
+++ src/App.js
@@ -1,63 +1,23 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
-import {
-  getAllPost,
-  getSinglePostByID,
-  getSinglePostWithComments,
-} from "./service/postAPI";
+
+import PostList from "./page/PostList";
+import PostListWithComments from "./page/PostListWithComments";
 
 function App() {
-  const [listOfPosts, setListOfPosts] = useState([]);
-
-  useEffect(() => {
-    (async function get() {
-      let posts = await getAllPost();
-      console.log("val", posts);
-      if (posts.length) {
-        setListOfPosts(posts);
-      }
-    })();
-  }, []);
-  async function handlePostOnClick(post_id) {
-    let post = await getSinglePostByID(post_id);
-    console.log("post", post);
-  }
-  async function handleViewCommentOnClick(post_id) {
-    let postWithComments = await getSinglePostWithComments(post_id);
-    console.log("post", postWithComments);
-  }
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <div>
-        {listOfPosts &&
-          listOfPosts.map((post, index) => {
-            return (
-              <div onClick={() => handlePostOnClick(post.id)}>
-                <span>{post.userId}</span>
-                <b>{post.title}</b>
-                <span>{post.body}</span>
-                <span onClick={() => handleViewCommentOnClick(post.id)}>
-                  view comments
-                </span>
-              </div>
-            );
-          })}
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <PostList />
+          </Route>
+          <Route path="/post/:postID">
+            <PostListWithComments />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
