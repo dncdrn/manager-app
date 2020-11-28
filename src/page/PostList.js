f053cd22f90330fae82b742788daa9f9a2d879ea
dncdrn <missnentin@gmail.com>
2020-11-28 13:59:19 +0800

    added loading screen in postlist page

--- src/page/PostList.js
+++ src/page/PostList.js
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from "react";
 import PostItem from "../components/PostItem";
 import { getAllPost } from "../service/postAPI";
+import LoadingScreen from "../components/LoadingScreen";
 
 export default function PostList() {
   const [listOfPosts, setListOfPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const posts = await getAllPost();
       setListOfPosts(posts);
+      setLoading(false);
     };
     fetchPosts();
   }, []);
+
   return (
     <>
-      <div className="title">Posts</div>
-      <div className="list-container">
-        {listOfPosts && listOfPosts.map((post) => <PostItem post={post} />)}
-      </div>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <div>
+          <div className="title">Posts</div>
+          <div className="list-container">
+            {listOfPosts && listOfPosts.map((post) => <PostItem post={post} />)}
+          </div>
+        </div>
+      )}
     </>
   );
 }
