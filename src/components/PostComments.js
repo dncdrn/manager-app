e5f9541618867b984dc441edb28c63ace46f67f0
dncdrn <missnentin@gmail.com>
2020-11-28 11:58:31 +0800

    added div for showing avatar

--- src/components/PostComments.js
+++ src/components/PostComments.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 export default function PostComments({ postComment }) {
+  const avatarInitial = postComment.name.charAt(0).toUpperCase();
   return (
     <div className="container comment">
       <div>
         <em>Comment</em>
       </div>
-      <div className="" data-initials="DA"></div>
+      <div className="avatar-initial">{avatarInitial}</div>
       <b>{postComment.name}</b>
       <div>{postComment.email}</div>
       <div>{postComment.body}</div>
