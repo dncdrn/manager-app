70ac8eff10dd35d4f5d00b241d4ea1b34c10d6fb
dncdrn <missnentin@gmail.com>
2020-11-28 13:59:27 +0800

    added loading screen in postlistcomments page

--- src/page/PostListWithComments.js
+++ src/page/PostListWithComments.js
@@ -7,6 +7,7 @@ import {
   getSinglePostByID,
   getSinglePostWithComments,
 } from "../service/postAPI";
+import LoadingScreen from "../components/LoadingScreen";
 
 export default function PostListWithComments() {
   let { postID } = useParams();
@@ -14,6 +15,7 @@ export default function PostListWithComments() {
   const [postDetail, setPostDetail] = useState();
   const [postComments, setPostComments] = useState();
   const [originalPostComments, setOriginalPostComments] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -25,6 +27,7 @@ export default function PostListWithComments() {
       const postWithComments = await getSinglePostWithComments(postID);
       setPostComments(postWithComments);
       setOriginalPostComments(postWithComments);
+      setLoading(false);
     };
 
     Promise.all([fetchPost(), fetchPostWithComment()]);
@@ -32,17 +35,25 @@ export default function PostListWithComments() {
 
   return (
     <div>
-      <div className="title">Post</div>
-      {postDetail && <PostItem post={postDetail} />}
-      <SearchBox
-        postComments={postComments}
-        setPostComments={setPostComments}
-        originalPostComments={originalPostComments}
-      />
-      {postComments &&
-        postComments.map((postComment) => (
-          <PostComments postComment={postComment} />
-        ))}
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <div>
+          <div className="title">Post</div>
+          {postDetail && <PostItem post={postDetail} />}
+          <SearchBox
+            postComments={postComments}
+            setPostComments={setPostComments}
+            originalPostComments={originalPostComments}
+          />
+          <div className="post-container">
+            {postComments &&
+              postComments.map((postComment) => (
+                <PostComments postComment={postComment} />
+              ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
