b7f49c8a497582b832ccfba50d3cfc8c6203b406
dncdrn <missnentin@gmail.com>
2020-11-30 14:46:35 +0800

    modify let to const

--- src/service/postAPI.js
+++ src/service/postAPI.js
@@ -1,15 +1,15 @@
 export async function getAllPost() {
-  let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   return await response.json();
 }
 export async function getSinglePostByID(post_id) {
-  let response = await fetch(
+  const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${post_id}`
   );
   return await response.json();
 }
 export async function getSinglePostWithComments(post_id) {
-  let response = await fetch(
+  const response = await fetch(
     ` https://jsonplaceholder.typicode.com/comments?postId=${post_id}`
   );
   return await response.json();
